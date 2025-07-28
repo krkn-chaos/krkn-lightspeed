703b18505919a82be3203e715b2ec8be89c0decc
Teju Gangisetty <tgangise@redhat.com>
2025-07-28 15:47:55 -0400

    Refactor PDF loading to common utils and update pipelines
    Signed-off-by: Teju Gangisetty <tgangise@redhat.com>

--- rag_pipelines/llama31_rag_pipeline.py
+++ rag_pipelines/llama31_rag_pipeline.py
@@ -1,13 +1,14 @@
 from langchain import hub
-from langchain_community.document_loaders import PyPDFLoader
+#from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+#from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain_community.embeddings import HuggingFaceEmbeddings
 from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.llms import Ollama
+from utils.pdf_loader import load_and_split_pdfs
 
 '''
 Code from langchain's Build a RAG App documentation
@@ -17,18 +18,13 @@ https://python.langchain.com/docs/tutorials/rag/
 def load_llama31_rag_pipeline(): 
 # load and chunk contents of thepytohnPDF
 
-    loader1 = PyPDFLoader("data/pod_scenarios.pdf")
-    loader2 = PyPDFLoader("data/Pod-Scenarios-using-Krknctl.pdf")
-    loader3 = PyPDFLoader("data/Pod-Scenarios-using-Krkn-hub.pdf")
-    loader4 = PyPDFLoader("data/Pod-Scenarios-using-Krkn.pdf")
-
-    docs1 = loader1.load()
-    docs2 = loader2.load()
-    docs3 = loader3.load()
-    docs4 = loader4.load()
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    all_splits = text_splitter.split_documents(docs1 + docs2+ docs3+ docs4)
+    pdf_paths = [
+        "data/pod_scenarios.pdf",
+        "data/Pod-Scenarios-using-Krknctl.pdf",
+        "data/Pod-Scenarios-using-Krkn-hub.pdf",
+        "data/Pod-Scenarios-using-Krkn.pdf"
+    ]
+    all_splits = load_and_split_pdfs(pdf_paths)
 
     # embed and store in vector database
     embedding_model = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
