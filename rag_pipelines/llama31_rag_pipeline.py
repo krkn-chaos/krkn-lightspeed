76dc3a934be700b89a39cb23cd8b2ac88ef5a027
Teju Gangisetty <tgangise@redhat.com>
2025-07-30 13:59:36 -0400

    removed unnecessary imports
    Signed-off-by: Teju Gangisetty <tgangise@redhat.com>

--- rag_pipelines/llama31_rag_pipeline.py
+++ rag_pipelines/llama31_rag_pipeline.py
@@ -1,9 +1,6 @@
 from langchain import hub
-#from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
-#from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-#from langchain_community.embeddings import HuggingFaceEmbeddings
 from langgraph.graph import START, StateGraph
 from typing_extensions import List, TypedDict
 from langchain_huggingface import HuggingFaceEmbeddings
