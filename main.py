1a059693adbcdd3444e39a49845e4f8e27af112d
Teju Gangisetty <tgangise@redhat.com>
2025-08-05 16:15:41 -0400

    called graph function in main
    Signed-off-by: Teju Gangisetty <tgangise@redhat.com>

--- main.py
+++ main.py
@@ -16,49 +16,20 @@ from langchain_community.llms import Ollama
 from rag_pipelines.granite_rag_pipeline import load_granite_rag_pipline
 from rag_pipelines.llama31_rag_pipeline import load_llama31_rag_pipeline
 from rag_pipelines.llama27_rag_pipeline import load_llama27_rag_pipeline
+from utils.state_graph import run_question_loop
 
 
 # UNCOMMENT THE CODE FOR THE MODEL THAT YOU ARE NOT USING BEFORE RUNNING
+
 
 #START OF LLAMA 3.1 MODEL LOGIC
 #llama 3.1
 graph = load_llama31_rag_pipeline()
 
 # run in a loop
-while True:
-    q = input("Ask a question (or type 'exit'): ")
-    if q.lower() in ["exit", "quit"]:
-        break
-    start_time = time.time()
-    result = graph.invoke({"question": q})
-    end_time = time.time()
-    duration = end_time - start_time
-    
-    print("Time taken:", round(duration, 2), "seconds")
+run_question_loop(graph)
 
-
-    print("\nAnswer:", result["answer"])
-
-
-'''#START OF LLAMA 2.7 MODEL LOGIC
-#llama 2.7
-graph = load_llama27_rag_pipeline()
-
-# run in a loop
-while True:
-    q = input("Ask a question (or type 'exit'): ")
-    if q.lower() in ["exit", "quit"]:
-        break
-    start_time = time.time()
-    result = graph.invoke({"question": q})
-    end_time = time.time()
-    duration = end_time - start_time
-    
-    print("Time taken:", round(duration, 2), "seconds")
-
-
-    print("\nAnswer:", result["answer"])
-#END OF LLAMA 2.7 MODEL LOGIC'''
+#END OF LLAMA 3.1 MODEL LOGIC
 
 
 '''#START OF GRANITE MODEL LOGIC
@@ -66,16 +37,18 @@ while True:
 graph = load_granite_rag_pipline()
 
 # run in a loop
-while True:
-    q = input("Ask a question (or type 'exit'): ")
-    if q.lower() in ["exit", "quit"]:
-        break
-    start_time = time.time()
-    result = graph.invoke({"question": q})
-    end_time = time.time()
-    duration = end_time - start_time
-    
-    print("Time taken:", round(duration, 2), "seconds")
+run_question_loop(graph)
+
+#END OF GRANITE MODEL LOGIC
+>>>>>>> ac1601d (called graph function in main)
 
 
-    print("\nAnswer:", result["answer"])
+#START OF LLAMA 2.7 MODEL LOGIC
+#llama 2.7
+graph = load_llama27_rag_pipeline()
+
+# run in a loop
+run_question_loop(graph)
+
+#END OF LLAMA 2.7 MODEL LOGIC
+'''
