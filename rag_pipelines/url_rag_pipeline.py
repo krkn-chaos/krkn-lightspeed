e40ed203170dd7557f9d8cf5a578ea80d2794ac5
Paige Patton <prubenda@redhat.com>
2025-08-14 16:09:06 -0400

    adding url splitting

---
+++ rag_pipelines/url_rag_pipeline.py
@@ -0,0 +1,32 @@
+from langchain import hub
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.llms import Ollama
+from utils.document_loader import load_and_split
+from utils.state_graph import *
+'''
+Code from langchain's Build a RAG App documentation
+https://python.langchain.com/docs/tutorials/rag/
+'''
+
+def load_url_llama_pipeline():
+    urls = [
+        "https://krkn-chaos.dev/docs/",
+        "https://krkn-chaos.dev/docs/krkn/",
+        "https://krkn-chaos.dev/docs/krkn-hub/",
+        "data"
+        ]
+    all_splits = load_and_split(urls)
+
+    # embed and store in vector database
+    embedding_model = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
+    vector_store = Chroma.from_documents(documents=all_splits, embedding=embedding_model)
+    # Define prompt for question-answering
+    # N.B. for non-US LangSmith endpoints, you may need to specify
+    # api_url="https://api.smith.langchain.com" in hub.pull.
+    prompt = hub.pull("rlm/rag-prompt")
+
+    llm = Ollama(model="llama3.1", base_url="http://127.0.0.1:11434")
+    
+    graph = build_state_graph(vector_store, prompt, llm)
+    return graph
--- utils/document_loader.py
+++ utils/document_loader.py
@@ -1,26 +1,44 @@
-from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader
+from langchain_community.document_loaders import PyPDFLoader, UnstructuredMarkdownLoader, UnstructuredURLLoader
+from langchain_pull_md.markdown_loader import PullMdLoader
+from langchain_docling import DoclingLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 
-def load_and_split_docs(folder_path="data"): 
-    docs = []
+def load_and_split_docs(folder_path="data"):
+
     file_paths = [
         os.path.join(root, f)
         for root, _, files in os.walk(folder_path)
         for f in files
         if f.endswith(".pdf") or f.endswith(".md")
     ]
-    for path in file_paths: 
-        ext = os.path.splitext(path)[1].lower()
-        if ext == ".pdf":
-            loader = PyPDFLoader(path)
-            docs.extend(loader.load())
-        elif ext == ".md": 
-            loader = UnstructuredMarkdownLoader(path)
-            docs.extend(loader.load())
     
+    return file_paths
+
+
+def load_and_split(file_paths):
+    docs = []
+    urls = []
+    for file in file_paths:
+        if "https" in file:
+            urls.append(file)
+        else:
+            files_in_folder = load_and_split_docs(file)
+            for path in files_in_folder:
+                ext = os.path.splitext(path)[1].lower()
+                if ext == ".pdf":
+                    loader = PyPDFLoader(path)
+                elif ext == ".md": 
+                    loader = UnstructuredMarkdownLoader(path)
+                docs.extend(loader.load())
+    if len(urls) > 0:
+        loader = UnstructuredURLLoader(urls=urls, show_progress_bar=True)
+        docs.extend(loader.load())
+
     text_splitter= RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap = 200
     )
-    return text_splitter.split_documents(docs)
+
+    split_docs = text_splitter.split_documents(docs)
+    return split_docs
